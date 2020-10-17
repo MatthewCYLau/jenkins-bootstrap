65449e51f8584002d23a35bb572791687888973f
Matthew Lau <matthewlau@matthews-mbp.mynet>
2020-10-17 15:24:23 +0100

    updated jobs groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,18 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/MatthewCYLau/gradle-spring-boot-api.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
